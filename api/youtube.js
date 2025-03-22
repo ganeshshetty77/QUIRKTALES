72df135929eabdaed471cd60bdc3978202411a4e
Ganesh Shetty <133388733+ganeshshetty77@users.noreply.github.com>
2025-03-22 18:16:51 +0000

    j

--- api/youtube.js
+++ api/youtube.js
@@ -4,23 +4,3 @@ export default function handler(req, res) {
       channelId: process.env.YOUTUBE_CHANNEL_ID
     });
   }
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("redirect-btn");
-
-    button.addEventListener("click", () => {
-        const videoId = button.getAttribute("data-video-id"); // Get the video ID from the button's data attribute
-        const youtubeAppLink = `youtube://www.youtube.com/watch?v=${videoId}`;
-        const youtubeWebLink = `https://www.youtube.com/watch?v=${videoId}`;
-        const isMobile = /Mobi|Android/i.test(navigator.userAgent);
-
-        if (isMobile) {
-            window.location.href = youtubeAppLink;
-            setTimeout(() => {
-                window.location.href = youtubeWebLink; // Fallback if app isn't installed
-            }, 1000);
-        } else {
-            window.open(youtubeWebLink, "_blank");
-        }
-    });
-});
-
