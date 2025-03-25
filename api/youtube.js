5455b2c7ffaf8d7c43ca602e801503a25427fa9a
Ganesh Shetty <133388733+ganeshshetty77@users.noreply.github.com>
2025-03-25 20:22:00 +0000

    c

--- api/youtube.js
+++ api/youtube.js
@@ -1,6 +1,9 @@
-export default function handler(req, res) {
-    res.json({
-      apiKey: process.env.YOUTUBE_API_KEY,
-      channelId: process.env.YOUTUBE_CHANNEL_ID
-    });
+export default async function handler(req, res) {
+  try {
+      const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${process.env.YOUTUBE_CHANNEL_ID}&key=${process.env.YOUTUBE_API_KEY}`);
+      const data = await response.json();
+      res.status(200).json(data);
+  } catch (error) {
+      res.status(500).json({ error: "Failed to fetch videos" });
   }
+}
